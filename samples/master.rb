07b7145610e1b6f1755206f758a56af0f90285d3
Ko-ichiro Sugiyama <32918854+sugiymki@users.noreply.github.com>
2025-12-31 15:30:40 +0900

    Update master.rb
    write_string -> print

--- samples/master.rb
+++ samples/master.rb
@@ -1,24 +1,13 @@
-# coding: utf-8
-
 #I2C 初期化
-i2c = I2C.new(22, 21)
+i2c = I2C.new()
 
 # LCD 初期化
 lcd = AQM0802A.new(i2c)
 
 # LCD に "Hello World" 表示
-lcd.cursor(1, 0)
-lcd.write_string("Hello!")
-
-# i2c で直接送信
+lcd.clear          #初期化
+str = "ESP"        #変数に値を代入
+lcd.cursor(0, 0)   
+lcd.print("Hello?!")
 lcd.cursor(0, 1)
-opcode = 0x40.chr
-i2c.write(0x3e, opcode + "from ESP")
-# <=> lcd.write_string("from ESP")
-sleep(10)
-
-# 適当な時間を表示
-lcd.cursor(0, 0)
-lcd.write_string(sprintf("%02d-%02d-%02d", 10, 30, 70))
-lcd.cursor(0, 1)
-lcd.write_string(sprintf("%02d:%02d:%02d", 1, 4, 9))
+lcd.print("from #{str}") #変数の埋め込み
