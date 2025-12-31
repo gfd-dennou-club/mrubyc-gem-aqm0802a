732186d7a0fd0e5d6cc885d2791b057685c5598a
Ko-ichiro Sugiyama <sugiyama@gfd-dennou.org>
2025-12-31 15:34:25 +0900

    write_string -> print

--- mrblib/aqm0802a.rb
+++ mrblib/aqm0802a.rb
@@ -20,12 +20,6 @@ class AQM0802A
     end
   end
 
-#  def setup
-#    lcd_write(0x00, [0x38, 0x39, 0x14, 0x70, 0x56, 0x6c])
-#    sleep(1)
-#    lcd_write(0x00, [0x38, 0x0c, 0x01])
-#  end
-
   def clear
     lcd_write(0x00, [0x01])
   end
@@ -34,7 +28,7 @@ class AQM0802A
     lcd_write(0x00, [0x80 + (0x40 * y + x)])
   end
 
-  def write_string(s)
+  def print(s)
     a = Array.new
     s.length.times do |n|
       a.push(s[n].ord)
